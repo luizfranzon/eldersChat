2b810e13def7ad28e63ce79fd37211b4c6099b69
Luiz <luizf@luizf.localdomain>
2022-07-22 22:12:27 -0300

    prototipo 1

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const cors = require("cors");
+
+const port = 3080;
+
+const app = express();
+const server = http.createServer(app);
+const io = require("socket.io")(server);
+const publicFolder = path.join(__dirname, "public");
+
+app.use(express.static(publicFolder));
+app.use(cors());
+app.set("views", publicFolder);
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+
+app.get("/", (req, res) => {
+    res.render("index.html");
+});
+
+let messages = [];
+
+io.on("connection", (socket) => {
+    console.log(`Novo socket conectado! ${socket.id}`);
+    socket.emit("previousMessages", messages);
+
+    socket.on("sendMessage", (data) => {
+        console.log(data)
+        messages.push(data);
+        socket.broadcast.emit("receivedMessage", data);
+    });
+});
+
+server.listen(port, () => {
+    console.log(`Servidor iniciado na porta: ${port}`);
+});
